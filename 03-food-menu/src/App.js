8b1c3b6b21128c9a201991604bc53b538611fe16
ookapratama <ookapratama2@gmail.com>
2023-12-11 23:11:00 +0800

    push view menu

--- 03-food-menu/src/App.js
+++ 03-food-menu/src/App.js
@@ -1,25 +1,81 @@
-import logo from "./logo.svg";
-import "./App.css";
+const App = () => {
+  const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
 
-function App() {
+  // console.log(pizzaData.map((item) => item));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Mini Project Pizza</h1>
       </header>
+      <Menu />
+      <footer>Copyright By Ookapratama</footer>
     </div>
   );
-}
+};
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Our Menu Pizza</h2>
+      <Content />
+      <Content />
+      <Content />
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <img src="pizzas/prosciutto.jpg" alt="" />
+      <h2>Pizza Prosciutto</h2>
+      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+    </div>
+  );
+};
 
 export default App;
